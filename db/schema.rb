2f158151897056cf11d82a6161623b4cd13d5e7d
akyhysa <m_akihisa8566@yahoo.co.jp>
2020-12-23 00:11:02 +0900

    add-admin-user-asothiation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_141511) do
+ActiveRecord::Schema.define(version: 2020_12_22_150856) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
