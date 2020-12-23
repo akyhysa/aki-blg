ae47a17b51fa660eda4456c60d9d79d9e463ae8e
akyhysa <m_akihisa8566@yahoo.co.jp>
2020-12-23 11:15:01 +0900

    edited-layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
 
   def home
     # redirect_to articles_path if logged_in?
+    @articles = Article.paginate(page: params[:page], per_page: 5)
+    @random = Article.order("RAND()").limit(6)
   end
 
   def about
