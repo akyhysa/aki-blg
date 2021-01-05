1ad4402ab291fd26314ca8b8579e40ac6d3d4893
akyhysa <m_akihisa8566@yahoo.co.jp>
2021-01-06 00:52:30 +0900

    edit-mximum

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,7 @@ class Report < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 100000 }
   mount_uploader :image, ImageUploader
 
 end 
