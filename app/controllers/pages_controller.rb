204e3021bf92902497742ca646620495e31d30d3
akyhysa <m_akihisa8566@yahoo.co.jp>
2021-01-05 17:21:21 +0900

    edit-api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,9 @@ class PagesController < ApplicationController
       # redirect_to reports_path if logged_in?
       @reports = Report.paginate(page: params[:page], per_page: 5)
       @random = Report.order("RAND()").limit(6)
+
       api = Rails.application.credentials.news_api:a16a9bc6a6df47fdb27cabf988017ecd
-      uri = "https://newsapi.org/v2/top-headlines?country=jp&apiKey=a16a9bc6a6df47fdb27cabf988017ecd"
+      uri = "http://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=a16a9bc6a6df47fdb27cabf988017ecd"
       article_serialized = open(uri).read
       @articles = JSON.parse(article_serialized)
     end
